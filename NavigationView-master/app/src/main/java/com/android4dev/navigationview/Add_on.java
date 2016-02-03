7f7fce469298b672e82773c187efc77df7e6c557
Farida Amila Husna <faridaamila@yahoo.co.id>
2016-02-03 15:29:09 +0700

    addon volley

--- NavigationView-master/app/src/main/java/com/android4dev/navigationview/Add_on.java
+++ NavigationView-master/app/src/main/java/com/android4dev/navigationview/Add_on.java
@@ -1,20 +1,68 @@
 package com.android4dev.navigationview;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 /**
  * Created by S. Harsono on 2/3/2016.
  */
 public class Add_on extends Fragment {
+    private ProgressDialog loading;
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.add_on, container, false);
+        getData();
         return v;
+    }
 
+    private void getData() {
+        loading = ProgressDialog.show(getActivity(), "Please wait...", "Fetching...", false, false);
 
+        String url = "http://subga.info/Assets/get_data/data_file_addon.php";
+
+        StringRequest stringRequest = new StringRequest(url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                loading.dismiss();
+                showJSON(response);
+            }
+        },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(getActivity(), error.toString(), Toast.LENGTH_LONG).show();
+                    }
+                });
+
+        RequestQueue requestQueue = Volley.newRequestQueue(getActivity());
+        requestQueue.add(stringRequest);
+    }
+
+    private void showJSON(String response){
+        String isi_freetext="";
+        try {
+            JSONObject jsonObject = new JSONObject(response);
+            JSONArray result = jsonObject.getJSONArray("result");
+            JSONObject collegeData = result.getJSONObject(0);
+            isi_freetext = collegeData.getString("isi_freetext");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        //tv.setText(isi_freetext);
     }
 
 }
